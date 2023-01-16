6d4c3f01bf15b5053c5c423faa99223383df2e2f
ShivaniGattewar <96229809+ShivaniGattewar@users.noreply.github.com>
2023-01-16 13:00:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, {useState,useEffect} from 'react';
+import './App.css';
+
+function App() {
+    const getMode = ()=>{
+        // return JSON.parse(localStorage.setItem("mode")) || false
+        const initialMode = localStorage.getItem("mode")
+        if (initialMode == null){
+            if(window.matchMedia("(prefers-color-scheme:light)").matches){
+                return true
+            }
+            else{
+                return false
+            }
+        }
+        else{
+            return JSON.parse( localStorage.getItem("mode"))
+        }
+    }
+ 
+    const [dark, setMode] = useState(getMode);
+    useEffect(()=>{
+        localStorage.setItem("mode",JSON.stringify(dark))
+    },[dark])
+   return(
+        <div className={dark ? "App dark-mode":"App"}>
+            <div className='nav'>
+                <label className="switch">
+                    <input 
+                    type="checkbox"
+                    checked={dark}
+                    onChange={() => setMode(!dark)}
+                    />
+                        <span className="slider round"></span>
+                </label>
+            </div>
+            <div className='content'>
+                 <h1>{dark?"Dark mode is on":"Light mode is on"}</h1>
+                 <p style={{fontSize:"20px"}}>Want to see some magic? press toggle button</p>
+            </div>
+        </div>
+    );
+    
+    }
+
+export default App;
